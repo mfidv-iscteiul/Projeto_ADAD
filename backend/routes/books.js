1ad199dc84eeca7e81e406d99a6e5dea1a5b2740
mfidv-iscteiul <145684155+mfidv-iscteiul@users.noreply.github.com>
2024-10-29 23:01:32 +0000

    Define endpoints de atualização de books e users

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,6 +2,21 @@ import express from "express";
 import db from "../db/config.js";
 const router = express.Router();
 
+router.post("/:id", (req, res) => {
+    let results = db.collection("books").updateOne(
+        {_id: req.params.id},
+        {$set: req.body}
+    )
 
+    res.send(results).status(200);
+})
+
+router.post("/top/:limit", (req, res) => {
+
+})
+
+router.post("/ratings/:order", (req, res) => {
+
+})
 
 export default router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,13 @@ import express from "express";
 import db from "../db/config.js";
 const router = express.Router();
 
+router.post("/:id", (req, res) => {
+    let results = db.collection("users").updateOne(
+        {_id: req.params.id},
+        {$set: req.body}
+    )
 
+    res.send(results).status(200);
+})
 
 export default router;
