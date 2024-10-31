6b46d5157503c4dffdbbc9d6adff438f4ba99e50
mfidv-iscteiul <145684155+mfidv-iscteiul@users.noreply.github.com>
2024-10-31 23:58:12 +0000

    Conclui endpoint 12

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -40,8 +40,32 @@ router.get("/top/:limit", async (req, res) => {
 })
 
 //Endpoint 12
-router.get("/ratings/:order", (req, res) => {
+router.get("/ratings/:order", async (req, res) => {
+    const order = req.params.order == "asc" ? 1 : -1;
+    
+    let results = await db.collection("users").aggregate([
+        {$unwind :
+            "$reviews"
+        },
+        {$group : {
+            _id: "$reviews.book_id",
+            number_of_reviews: {$count: {}}
+        }},
+        {$sort: { 
+            number_of_reviews: order
+        }},
+        {$lookup: {
+            from: "books",
+            localField: "_id",
+            foreignField: "_id",
+            as: "livro"
+        }},
+        { $project: {
+            _id:0
+        }}
+    ]).toArray();
 
+    res.send(results).status(200);
 })
 
 export default router;
