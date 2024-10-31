af42757ba76f152d67645156949ce03827884069
mfidv-iscteiul <145684155+mfidv-iscteiul@users.noreply.github.com>
2024-10-31 00:23:49 +0000

    Realiza parcialmente o endpoint 11

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -2,20 +2,36 @@ import express from "express";
 import db from "../db/config.js";
 const router = express.Router();
 
+//Endpoint 9
 router.post("/:id", (req, res) => {
     let results = db.collection("books").updateOne(
-        {_id: req.params.id},
+        {_id: parseInt(req.params.id)},
         {$set: req.body}
     )
 
     res.send(results).status(200);
 })
 
-router.post("/top/:limit", (req, res) => {
+//Endpoint 11
+router.get("/top/:limit", async (req, res) => {
+    let results = await db.collection("users").aggregate([
+        {$unwind :
+            "$reviews"
+        },
+        {$group : {
+            _id: "$reviews.book_id",
+            average_score: {$avg: "$reviews.score"}
+        }},
+        {$sort: { 
+            average_score: -1 
+        }}
+    ]).limit(parseInt(req.params.limit)).toArray();
 
+    res.send(results).status(200);
 })
 
-router.post("/ratings/:order", (req, res) => {
+//Endpoint 12
+router.get("/ratings/:order", (req, res) => {
 
 })
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "../db/config.js";
 const router = express.Router();
 
+//Endpoint 10
 router.post("/:id", (req, res) => {
     let results = db.collection("users").updateOne(
         {_id: req.params.id},
