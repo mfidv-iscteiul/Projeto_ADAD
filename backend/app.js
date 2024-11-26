7e28fd04ef96c11072319925954966c24efcdc5a
jpnoa1 <145027688+jpnoa1@users.noreply.github.com>
2024-11-26 10:32:44 +0000

    Mudança do app.js por causa do frontEnd

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,15 @@ import books from "./routes/books.js";
 import users from "./routes/users.js";
 import comments from "./routes/comments.js";
 import livrarias from "./routes/livrarias.js";
+import cors from "cors"
+
+
 
 const app = express()
 const port = 3000
 
 app.use(express.json());
+app.use(cors())
 // Load the routes
 app.use("/books", books);
 app.use("/users", users);
