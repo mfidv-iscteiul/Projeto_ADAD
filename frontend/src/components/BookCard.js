25d093fc03a32c20fa63f3409f2fe6c1f34a5d76
mfidv-iscteiul <145684155+mfidv-iscteiul@users.noreply.github.com>
2024-11-27 21:11:49 +0000

    Edita BookCard component

--- frontend/src/components/BookCard.js
+++ frontend/src/components/BookCard.js
@@ -1,16 +1,25 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
 function BookCard(props) {
   return (
     <Card style={{ width: '18rem' }} className="mb-3">
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>
-          _id: {props._id}
-        </Card.Text>
-        <Button href={"/book/" + props._id} variant="outline-primary">Open Book</Button>
+      <Card.Body style={{display: "flex",  flexDirection: "column"}}>
+        <Card.Img variant="top" src={props.thumbnailUrl}></Card.Img>
+        <Card.Title style={{textAlign: 'center', marginBottom: 1.5 + "em"}}>{props.title}</Card.Title>
+        <Card.Subtitle style={{marginBottom: 0.5 + "em"}}>By:</Card.Subtitle>
+        <ListGroup variant="flush">
+          {props.authors &&
+            props.authors.map((author) => {
+              return <ListGroupItem>{author}</ListGroupItem>;
+          })}
+        </ListGroup>
+        <h5 style={{textAlign: 'center', fontWeight: "bold", marginTop: "auto"}}>{props.price}€</h5>
       </Card.Body>
+      <Card.Footer>
+      <Button href={"/book/" + props._id} variant="outline-primary">Open Book</Button></Card.Footer>
     </Card>
   );
 }
