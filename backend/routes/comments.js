7ebde1527c939df2813eac797d97a8f85ec95123
jpnoa1 <145027688+jpnoa1@users.noreply.github.com>
2024-12-03 12:04:42 +0000

    Corrige falta de VerifyID

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -2,6 +2,7 @@ import express from "express";
 import db from "../db/config.js";
 const router = express.Router();
 
+import { VerifyID } from './books.js'; // para ir buscar a funcao desenvolvida nos books
 
 //endpoint 18
 
@@ -24,38 +25,22 @@ router.post("/", async (req, res) => {
 //endpoint 19
 
 router.delete("/:id", async (req, res) => {
-    const commentID = req.params.id;
     try {
-        if (isNaN(commentID)) { //para verificar se é um objectId
-            const result = await db.collection("comments").deleteOne(
-                { 
-                _id: new ObjectId(commentID)
-             });
+        
+        const commentID = VerifyID(req.params.id);
 
-            if (result.deletedCount === 1) {
-                res.send(result).status(200);
-            }
-            else {   // comentario nao encontrado
+        const result = await db.collection("comments").deleteOne({ 
+            _id: commentID
+        });
 
-                res.status(404).json({ message: "comentario não encontrado." });
-            }
+        if (result.deletedCount === 1) {
+            res.send(result).status(200);
+        }else{   // comentario nao encontrado
+            res.status(404).json({ message: "comentario não encontrado." });
         }
-        else {//se o Id for um numero
-            const result = await db.collection("comments").deleteOne(
-                { 
-                _id: parseInt(commentID) 
-            });
 
-            if (result.deletedCount === 1) { // comentario removido com sucesso
-                res.send(result).status(200);
-            } else {
-                // Comentário não encontrado
-                res.status(404).json({ message: "comentario não encontrado." });
-            }
-        }
     } catch (error) {
         res.status(500).json({ message: "Erro ao remover comentario" });
-
     }
 });
 
