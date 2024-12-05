6236dd71aeb3c8282f360ebdd25d15ec202c7eb3
gfbas-iscte <gfbas@iscte-iul.pt>
2024-12-05 18:24:28 +0000

    Cria a página /book/:id e apresenta info dos livros

--- frontend/src/pages/Book.js
+++ frontend/src/pages/Book.js
@@ -1,33 +1,50 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { openContractCall } from '@stacks/connect';
 import {
-  bufferCV,
+	bufferCV,
 } from '@stacks/transactions';
 import { utf8ToBytes } from '@stacks/common';
 import { userSession } from '../auth';
-const bytes = utf8ToBytes('foo'); 
+const bytes = utf8ToBytes('foo');
 const bufCV = bufferCV(bytes);
 
 export default function App() {
-  let params = useParams();
-  let [book, setBook] = useState([]);
+	let params = useParams();
+	let [book, setBook] = useState([]);
 
- 
+	const getBook = async (id) => {
+		try {
+			const response = await fetch(`http://localhost:3000/books/id/${id}`, {
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application / json',
+				},
+			});
+			const data = await response.json();
+			setBook(data);
+		} catch (error) {
+			console.error('Error:', error);
+		}
+	};
 
-  useEffect(() => {
-    let id = params.id;
-    console.log(id);
-    //getBook(params.id);
+	useEffect(() => {
+		let id = params.id;
+		console.log(id);
+		getBook(id);
 
-  }, []);
+	}, [params.id]);
 
-  return (
-    <div className="container pt-5 pb-5">
-      <h2>Book page</h2>
-      <p>use /book/:id endpoint</p>
+	return (
+		<div className="container pt-5 pb-5">
+			<h2>Book page</h2>
+			<img src={book.thumbnailUrl} alt={book.title} />
+			<p><strong>Title:</strong> {book.title}</p>
+			<p><strong>Id:</strong> {book._id}</p>
+			<p><strong>Nº páginas:</strong> {book.pageCount}</p>
+			<p><strong>Descrição:</strong> {book.shortDescription}</p>
+			<p><strong>Preço:</strong> {book.price}</p>
 
-      
-    </div>
-  )
+		</div>
+	)
 }
