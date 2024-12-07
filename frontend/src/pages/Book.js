5ae041323ab14785eeb78cb87fffdf4b674565fc
mfidv-iscteiul <145684155+mfidv-iscteiul@users.noreply.github.com>
2024-12-07 17:17:17 +0000

    Adiciona score e comentários à pagina do livro

--- frontend/src/pages/Book.js
+++ frontend/src/pages/Book.js
@@ -33,20 +33,21 @@ export default function App() {
 
 	return (
 		<div className="container pt-5 pb-5">
-			<div style={{display: "flex", justifyContent:"center"}}>
+			<div style={{ display: "flex", justifyContent: "center" }}>
 				<div>
-					<img src={book.thumbnailUrl} alt={book.title} style={{width: "300px", flexShrink: "0"}}/>
+					<img src={book.thumbnailUrl} alt={book.title} style={{ width: "300px", flexShrink: "0" }} />
 				</div>
-				<div style={{marginLeft: "40px", paddingBottom: "16px" , display: "flex",  flexDirection: "column"}}>
+				<div style={{ marginLeft: "40px", paddingBottom: "16px", display: "flex", flexDirection: "column" }}>
 					<h1><strong>{book.title}</strong></h1>
 					<h5>De</h5>
-					<ul style={{listStyle: "none"}}>
+					<ul style={{ listStyle: "none" }}>
 						{book.authors &&
 							book.authors.map((author) => {
 								return <li key={i++}>{author}</li>;
 							})}
 					</ul>
-					<h3 style={{marginTop:"auto"}}><strong>{book.price}€</strong></h3>
+					<p style={{ fontSize: "1.3em" }}><span style={{ fontSize: "1.5em" }}><strong>{Math.round(book.averageScore * 100) / 100}</strong></span>/5 estrelas</p>
+					<h3 style={{ marginTop: "auto" }}><strong>{book.price}€</strong></h3>
 				</div>
 			</div>
 			<hr></hr>
@@ -56,7 +57,23 @@ export default function App() {
 			<p><strong>ISBN:</strong> {book.isbn}</p>
 			<p><strong>Nº páginas:</strong> {book.pageCount}</p>
 			<p><strong>Data de publicação:</strong> {new Date(book.publishedDate).toLocaleDateString()}</p>
-
+			<p><strong>Categorias:</strong></p>
+			<ul>
+						{book.categories &&
+							book.categories.map((categorie) => {
+								return <li key={i++}>{categorie}</li>;
+							})}
+			</ul>
+			<h2>Comentários</h2>
+			<ul style={{ listStyle: "none" }}>
+						{book.comments &&
+							book.comments.map((comment) => {
+								return <li key={comment._id}>
+									<h6><strong>User ID: {comment.user_id}</strong></h6>
+									<p>{comment.comment}</p>
+								</li>;
+							})}
+					</ul>
 		</div>
 	)
 }
